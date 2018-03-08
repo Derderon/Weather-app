3211da37e42e13eb8cd5ea22f8f60a1db54a3324
Derderon <36231878+Derderon@users.noreply.github.com>
2018-03-08 15:11:21 +0100

    with maps

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = "166de3590f883ff4b2c508090e962919";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
--- src/containers/Search_bar.js
+++ src/containers/Search_bar.js
@@ -20,7 +20,7 @@ class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     // we need to go and fetch weather data
-    this.props.fetchWeather(this.state.term);
+    this.props.fetchWeather(this.state.term, this.state.term);
     this.setState({ term: '' });
   }
 
